741b51814868d77a5e5bc72728b67d2534a8d416
nikul1ch <nikulin@netkam.ru>
2022-08-22 14:45:32 +0300

    Программа, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.

--- seminar1.4/Program.cs
+++ seminar1.4/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите целое число больше 1: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+if (number < 2)
+{
+    Console.WriteLine($"Введённое число {number} меньше 2");
+}
+else
+{
+    Console.WriteLine($"Четные числа от 1 до {number}(включительно):");
+    while (count <= number)
+    {
+        Console.Write($"{count} ");
+        count += 2;
+    }
+}
