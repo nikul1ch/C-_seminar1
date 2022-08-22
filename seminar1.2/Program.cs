3cf589c4ca35167de9dde96e8bfb82dae9e81667
nikul1ch <nikulin@netkam.ru>
2022-08-22 14:23:22 +0300

    Программа, которая принимает на вход три числа и выдаёт максимальное из этих чисел

--- seminar1.2/Program.cs
+++ seminar1.2/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число число: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 > number2 && number1 > number3)
+{
+    Console.WriteLine($"Число {number1} больше чисел {number2} и {number3}");
+}
+else if (number2 > number1 && number2 > number3)
+{
+    Console.WriteLine($"Число {number2} больше чисел {number1} и {number3}");
+}
+else Console.WriteLine($"Число {number3} больше чисел {number1} и {number2}");
