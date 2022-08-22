0d3c62380d61c4bf8efa90b527cff63217193ad1
nikul1ch <nikulin@netkam.ru>
2022-08-22 14:30:15 +0300

    Программа, которая на вход принимает число и выдаёт, является ли число чётным

--- seminar1.3/Program.cs
+++ seminar1.3/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 == 0)
+{
+    Console.WriteLine($"Число {number} четное");
+}
+else Console.WriteLine($"Число {number} нечетное");
